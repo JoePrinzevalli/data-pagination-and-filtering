7b6333eb6cf1d88c25fc595cfab480091c593588
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-02-11 20:32:18 -0500

    created showPage function
    -next create addPagination function

--- neccessary-files/js/script.js
+++ neccessary-files/js/script.js
@@ -17,36 +17,33 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+const items = 9;
+
 const showPage = (list, page) => {
-   // for (let i = 0; i < list.length; i++) {
-   //    let items = list[i];                //is this right? shoudl i do somehting else//
-   //    return items;
-   // }
-   const startIndex = (page * items) - items; //what do the 'items' represent
+   const startIndex = (page * items) - items; 
    const endIndex = page * items;
    const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
-         const newLi = document.createElement('li');
-         newLi.classList.add('student-item cf');
-         const newDivStudent = document.createElement('div');
-         newDivStudent.classList.add('student-details');
-         const newImg = document.createElement('img'); //add an alt text to this at the end***//
-         newImg.classList.add('avatar');
-         const newH3 = document.createElement('h3');
-         const newSpanEmail = document.createElement('span'); 
-         newSpanEmail.classList.add('email');
-         const newDivJoined = document.createElement('div');
-         newDivJoined.classList.add('joined-details');
-         const newSpanDate = document.createElement('span');
-         newSpanDate.classList.add('date');
+         let showStudent =  
+         `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>`;
 
-         studentList.insertAdjacentElement('beforeend', `${newLi}`);
-      }
+       studentList.insertAdjacentHTML('beforeend', showStudent);
+      };
+   };
+};
 
-   }
-}
+showPage(data,1);
 
 
 
@@ -55,6 +52,17 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+const addPagination = list => {
+
+};
 
 
 // Call functions
+
+
+
+
+
+
+
+
